9a5c0efa4d6896f28936b5c798b5a7d0fffa46ed
amrrs <amrrs@outlook.com>
2018-02-28 01:15:10 +0530

    updated with requests

--- Indian Blog List Extraction/blogs_extractions.py
+++ Indian Blog List Extraction/blogs_extractions.py
@@ -1,19 +1,23 @@
-import urllib, urllib2
+import requests
 from bs4 import BeautifulSoup, Comment
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
 
 url='https://indianbloggers.org/'
-content = urllib2.urlopen(url).read()
+content = requests.get(url).text
 
-soup = BeautifulSoup(content, "html.parser")
+
 
 #initalizing an empty dictionary that would be written as Pandas Dataframe and then CSV
 d = {'title':[],'links':[]}
 
 #initializing blog hosting category
 cat = {'blogspot':0,'wordpress':0,'others':0}
+
+
+soup = BeautifulSoup(content, "html.parser")
+
 
 for link in soup.find_all('a',):
     if len(link.text.strip()) > 1 and bool(re.match('^http',link['href'])) and not bool(re.search('indianbloggers|twitter|facebook',link['href'])):
@@ -29,6 +33,9 @@ for link in soup.find_all('a',):
         #d['len'].append(len(link.text.strip()))
     
 blog_list = pd.DataFrame(d).set_index('title')
+
+
+print(blog_list.head())
 
 blog_list.to_csv('blog_list.csv', encoding='utf-8')
 
